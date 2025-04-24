57a8c2ce7b7c8a91e5d8f600ea45fd8f925b72d8
kuman514 <hadjadj0@gmail.com>
2025-04-25 03:00:18 +0900

    draft: add rendering image

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
-import { Application } from 'pixi.js';
+import { Application, Assets, Sprite } from 'pixi.js';
 
 (() => {
   const mainElement = document.querySelector('main');
   (async () => {
+    // Await the app initializing.
     const pixiJsApp = new Application();
     await pixiJsApp.init({ background: '#22b14c', resizeTo: window });
     mainElement.appendChild(pixiJsApp.canvas);
+
+    // Images load asynchronously. So, to get texture, we need to await the assets to be loaded.
+    const texture = await Assets.load(
+      'https://pbs.twimg.com/media/GNe66_yaMAMCshU.jpg'
+    );
+    const koishi = new Sprite(texture);
+
+    // Set sprite position and size.
+    koishi.anchor.set(0.5);
+    koishi.x = pixiJsApp.screen.width / 2;
+    koishi.y = pixiJsApp.screen.height / 2;
+    koishi.width /= 2;
+    koishi.height /= 2;
+
+    // Append to the stage.
+    pixiJsApp.stage.addChild(koishi);
   })();
 })();
