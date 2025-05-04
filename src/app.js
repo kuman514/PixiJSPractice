a99c9fb543949d6af2f586e1a8e45d39040f289b
kuman514 <hadjadj0@gmail.com>
2025-05-05 01:56:54 +0900

    refactor: make mountain graphics into array

--- src/app.js
+++ src/app.js
@@ -11,64 +11,66 @@ import { setup } from './setup/setup';
     addStars({ pixiJsApp, amount: 20 });
     addMoon({ pixiJsApp });
 
-    const mountainGraphics1 = addMountains({
-      pixiJsApp,
-      mountains: [
-        {
-          startX: pixiJsApp.screen.width / 4,
-          startY: pixiJsApp.screen.height,
-          width: pixiJsApp.screen.width / 2,
-          height: (pixiJsApp.screen.height * 4) / 5,
-          color: '#7e818f',
-        },
-        {
-          startX: 0,
-          startY: pixiJsApp.screen.height,
-          width: pixiJsApp.screen.width / 2,
-          height: pixiJsApp.screen.height / 2,
-          color: '#c1c0c2',
-        },
-        {
-          startX: pixiJsApp.screen.width / 2,
-          startY: pixiJsApp.screen.height,
-          width: pixiJsApp.screen.width / 2,
-          height: (pixiJsApp.screen.height * 2) / 3,
-          color: '#8c919f',
-        },
-      ],
-    });
-    const mountainGraphics2 = addMountains({
-      pixiJsApp,
-      mountains: [
-        {
-          startX: pixiJsApp.screen.width / 4,
-          startY: pixiJsApp.screen.height,
-          width: pixiJsApp.screen.width / 2,
-          height: (pixiJsApp.screen.height * 4) / 5,
-          color: '#7e818f',
-        },
-        {
-          startX: 0,
-          startY: pixiJsApp.screen.height,
-          width: pixiJsApp.screen.width / 2,
-          height: pixiJsApp.screen.height / 2,
-          color: '#c1c0c2',
-        },
-        {
-          startX: pixiJsApp.screen.width / 2,
-          startY: pixiJsApp.screen.height,
-          width: pixiJsApp.screen.width / 2,
-          height: (pixiJsApp.screen.height * 2) / 3,
-          color: '#8c919f',
-        },
-      ],
-    });
+    const mountainGraphics = [
+      addMountains({
+        pixiJsApp,
+        mountains: [
+          {
+            startX: pixiJsApp.screen.width / 4,
+            startY: pixiJsApp.screen.height,
+            width: pixiJsApp.screen.width / 2,
+            height: (pixiJsApp.screen.height * 4) / 5,
+            color: '#7e818f',
+          },
+          {
+            startX: 0,
+            startY: pixiJsApp.screen.height,
+            width: pixiJsApp.screen.width / 2,
+            height: pixiJsApp.screen.height / 2,
+            color: '#c1c0c2',
+          },
+          {
+            startX: pixiJsApp.screen.width / 2,
+            startY: pixiJsApp.screen.height,
+            width: pixiJsApp.screen.width / 2,
+            height: (pixiJsApp.screen.height * 2) / 3,
+            color: '#8c919f',
+          },
+        ],
+      }),
+      addMountains({
+        pixiJsApp,
+        mountains: [
+          {
+            startX: pixiJsApp.screen.width / 4,
+            startY: pixiJsApp.screen.height,
+            width: pixiJsApp.screen.width / 2,
+            height: (pixiJsApp.screen.height * 4) / 5,
+            color: '#7e818f',
+          },
+          {
+            startX: 0,
+            startY: pixiJsApp.screen.height,
+            width: pixiJsApp.screen.width / 2,
+            height: pixiJsApp.screen.height / 2,
+            color: '#c1c0c2',
+          },
+          {
+            startX: pixiJsApp.screen.width / 2,
+            startY: pixiJsApp.screen.height,
+            width: pixiJsApp.screen.width / 2,
+            height: (pixiJsApp.screen.height * 2) / 3,
+            color: '#8c919f',
+          },
+        ],
+      }),
+    ];
     // Set x offset of another mountain graphics
-    mountainGraphics2.x = pixiJsApp.screen.width;
+    mountainGraphics[1].x = pixiJsApp.screen.width;
 
     animateMountains({
       pixiJsApp,
-      graphics: [mountainGraphics1, mountainGraphics2],
+      graphics: mountainGraphics,
     });
   })();
 })();
