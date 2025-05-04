20837f973cc50ea2019d38ce5d4dc03c1c3b721d
kuman514 <hadjadj0@gmail.com>
2025-05-04 22:36:51 +0900

    draft: add & apply mountains

--- src/app.js
+++ src/app.js
@@ -1,15 +1,44 @@
 import { addMoon } from './graphics/moon';
+import { addMountains, animateMountains } from './graphics/mountain';
 import { addStars } from './graphics/star';
 import { pixiJsApp } from './setup/app-init';
-import { preload } from './setup/preload';
 import { setup } from './setup/setup';
 
 (() => {
   (async () => {
     await setup();
-    await preload();
 
     addStars({ pixiJsApp, amount: 20 });
     addMoon({ pixiJsApp });
+    const mountainGraphics = addMountains({
+      pixiJsApp,
+      mountains: [
+        {
+          startX: pixiJsApp.screen.width / 4,
+          startY: pixiJsApp.screen.height,
+          width: pixiJsApp.screen.width / 2,
+          height: (pixiJsApp.screen.height * 4) / 5,
+          color: '#7e818f',
+        },
+        {
+          startX: 0,
+          startY: pixiJsApp.screen.height,
+          width: pixiJsApp.screen.width / 2,
+          height: pixiJsApp.screen.height / 2,
+          color: '#c1c0c2',
+        },
+        {
+          startX: pixiJsApp.screen.width / 2,
+          startY: pixiJsApp.screen.height,
+          width: pixiJsApp.screen.width / 2,
+          height: (pixiJsApp.screen.height * 2) / 3,
+          color: '#8c919f',
+        },
+      ],
+    });
+    animateMountains({
+      pixiJsApp,
+      graphics: mountainGraphics,
+    });
   })();
 })();
---
+++ src/graphics/mountain.js
@@ -0,0 +1,41 @@
+import { Graphics } from 'pixi.js';
+
+function addMountain({ graphics, startX, startY, width, height, color }) {
+  graphics
+    .moveTo(startX, startY)
+    .bezierCurveTo(
+      startX + width / 2,
+      startY - height,
+      startX + width / 2,
+      startY - height,
+      startX + width,
+      startY
+    )
+    .fill({ color });
+
+  return graphics;
+}
+
+export function addMountains({ pixiJsApp, mountains }) {
+  const graphics = mountains.reduce(
+    (prevGraphics, mountain) =>
+      addMountain({
+        ...mountain,
+        graphics: prevGraphics,
+      }),
+    new Graphics()
+  );
+
+  pixiJsApp.stage.addChild(graphics);
+  return graphics;
+}
+
+export function animateMountains({ pixiJsApp, graphics }) {
+  pixiJsApp.ticker.add((time) => {
+    const dx = time.deltaTime * 0.5;
+    graphics.x -= dx;
+    if (graphics.x <= -pixiJsApp.screen.width) {
+      graphics.x += pixiJsApp.screen.width * 2;
+    }
+  });
+}
