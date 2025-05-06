1014456bf678fb5e6c9f800cd879701216638d78
kumankoishi.py <7iggif@naver.com>
2025-05-07 04:17:24 +0900

    draft: add mouse tracking & spinning icons

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { Container } from 'pixi.js';
 import { generateHoshino } from './entities/hoshino';
 import { generateKoishi } from './entities/koishi';
 import { PixiJsAppSingleton } from './shared/setup';
@@ -6,23 +7,53 @@ import { PixiJsAppSingleton } from './shared/setup';
   (async () => {
     const pixiJsApp = await PixiJsAppSingleton.getPixiJsApp();
 
-    const koishiIcon = generateKoishi({
-      x: Math.random() * pixiJsApp.screen.width,
-      y: Math.random() * pixiJsApp.screen.height,
-    });
-    pixiJsApp.stage.addChild(koishiIcon);
+    const icons = [
+      generateKoishi({
+        x: -120,
+        y: -120,
+      }),
+      generateHoshino({
+        x: 120,
+        y: -120,
+      }),
+      generateHoshino({
+        x: -120,
+        y: 120,
+      }),
+      generateKoishi({
+        x: 120,
+        y: 120,
+      }),
+    ];
 
-    const hoshinoIcon = generateHoshino({
-      x: Math.random() * pixiJsApp.screen.width,
-      y: Math.random() * pixiJsApp.screen.height,
+    const iconContainer = new Container();
+    iconContainer.pivot.x = iconContainer.width / 2;
+    iconContainer.pivot.y = iconContainer.height / 2;
+    iconContainer.addChild(...icons);
+
+    let mouseX = pixiJsApp.screen.width / 2;
+    let mouseY = pixiJsApp.screen.height / 2;
+
+    // This is needed for interaction
+    pixiJsApp.stage.eventMode = 'static';
+    pixiJsApp.stage.hitArea = pixiJsApp.screen;
+    pixiJsApp.stage.on('mousemove', (event) => {
+      mouseX = event.global.x;
+      mouseY = event.global.y;
     });
-    pixiJsApp.stage.addChild(hoshinoIcon);
+
+    pixiJsApp.stage.addChild(iconContainer);
 
     pixiJsApp.ticker.add((time) => {
       const deltaMs = time.deltaMS;
 
-      koishiIcon.rotation += (deltaMs / 1000) * 2 * Math.PI;
-      hoshinoIcon.rotation += (deltaMs / 1000) * 2 * Math.PI;
+      iconContainer.x = mouseX;
+      iconContainer.y = mouseY;
+
+      iconContainer.rotation -= (deltaMs / 1000) * 2 * Math.PI;
+      icons.forEach((icon) => {
+        icon.rotation += (deltaMs / 1000) * 2 * Math.PI;
+      });
     });
   })();
 })();
