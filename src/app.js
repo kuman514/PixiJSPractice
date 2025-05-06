38feae315edef65fca01012252474452814daa24
kumankoishi.py <7iggif@naver.com>
2025-05-07 05:50:21 +0900

    refactor: move tracker codes to new module

--- src/app.js
+++ src/app.js
@@ -1,58 +1,41 @@
-import { Container } from 'pixi.js';
-
 import { generateHoshino } from './entities/hoshino';
 import { generateKoishi } from './entities/koishi';
 import { PixiJsAppSingleton } from './shared/setup';
+import { generateIconTracker } from './features/icon-tracker';
 
 (() => {
   (async () => {
     const pixiJsApp = await PixiJsAppSingleton.getPixiJsApp();
 
-    const iconContainer = new Container();
-    iconContainer.pivot.x = iconContainer.width / 2;
-    iconContainer.pivot.y = iconContainer.height / 2;
-    iconContainer.addChild(
-      generateKoishi({
-        x: -120,
-        y: -120,
-      }),
-      generateHoshino({
-        x: 120,
-        y: -120,
-      }),
-      generateHoshino({
-        x: -120,
-        y: 120,
-      }),
-      generateKoishi({
-        x: 120,
-        y: 120,
-      })
-    );
-
-    let mouseX = pixiJsApp.screen.width / 2;
-    let mouseY = pixiJsApp.screen.height / 2;
-
     // This is needed for interaction
     pixiJsApp.stage.eventMode = 'static';
     pixiJsApp.stage.hitArea = pixiJsApp.screen;
-    pixiJsApp.stage.on('mousemove', (event) => {
-      mouseX = event.global.x;
-      mouseY = event.global.y;
+
+    const iconTracker = await generateIconTracker({
+      initIcons: [
+        generateKoishi({
+          x: -120,
+          y: -120,
+        }),
+        generateHoshino({
+          x: 120,
+          y: -120,
+        }),
+        generateHoshino({
+          x: -120,
+          y: 120,
+        }),
+        generateKoishi({
+          x: 120,
+          y: 120,
+        }),
+      ],
+      initPosition: {
+        x: pixiJsApp.screen.width / 2,
+        y: pixiJsApp.screen.height / 2,
+      },
     });
-    pixiJsApp.stage.addChild(iconContainer);
 
-    pixiJsApp.ticker.add((time) => {
-      const deltaMs = time.deltaMS;
-
-      iconContainer.x = mouseX;
-      iconContainer.y = mouseY;
-
-      iconContainer.scale.set(Math.abs(Math.sin(iconContainer.rotation / 4)));
-      iconContainer.rotation -= (deltaMs / 1000) * 2 * Math.PI;
-      iconContainer.children.forEach((icon) => {
-        icon.rotation += (deltaMs / 1000) * 3 * Math.PI;
-      });
-    });
+    pixiJsApp.stage.addChild(iconTracker);
   })();
 })();
---
+++ src/features/icon-tracker.js
@@ -0,0 +1,30 @@
+import { Container } from 'pixi.js';
+
+import { PixiJsAppSingleton } from '../shared/setup';
+
+export async function generateIconTracker({ initIcons, initPosition }) {
+  const container = new Container();
+
+  container.pivot.x = container.width / 2;
+  container.pivot.y = container.height / 2;
+
+  container.addChild(...initIcons);
+
+  container.position.copyFrom(initPosition);
+
+  const pixiJsApp = await PixiJsAppSingleton.getPixiJsApp();
+  pixiJsApp.ticker.add((time) => {
+    const deltaMs = time.deltaMS;
+
+    container.scale.set(Math.abs(Math.sin(container.rotation / 4)));
+    container.rotation -= (deltaMs / 1000) * 2 * Math.PI;
+    container.children.forEach((icon) => {
+      icon.rotation += (deltaMs / 1000) * 3 * Math.PI;
+    });
+  });
+  pixiJsApp.stage.on('mousemove', (event) => {
+    container.position.copyFrom(event.global);
+  });
+
+  return container;
+}
